d1c569c69db729e8cf14edd44bab4484acce238a
dominatrixx-1 <smkaraca06@gmail.com>
2022-07-27 05:12:30 +0300

    set interval

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 //jshint esversion:6
 
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const http = require("http");
+const app = express();
 
-var app = express();
 
+setInterval(function() {
+    http.get("https://fairydevmother.herokuapp.com/");
+}, 300000); // every 5 minutes (300000)
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
